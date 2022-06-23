8d0d5daf83d6c243eb66bb8a9e596cac5234c1be
Alexander <the7alexander7@gamil.com>
2022-06-23 17:25:26 +0300

    make first tests

---
+++ src/capitalize.js
@@ -0,0 +1,11 @@
+const capitalize = (text) => {
+  if (text === '') {
+    return '';
+  }
+
+  const firstChar = text[0].toUpperCase();
+  const restString = text.slice(1);
+  return `${firstChar}${restString}`;
+};
+
+export default capitalize;
