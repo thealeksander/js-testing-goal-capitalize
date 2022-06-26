f18559f7d9543a2c6165807ac24519af979bfbd3
Alexander <the7alexander7@gamil.com>
2022-06-26 21:46:27 +0300

    install power-asset

--- src/capitalize.js
+++ src/capitalize.js
@@ -2,7 +2,6 @@ const capitalize = (text) => {
   if (text === '') {
     return '';
   }
-
   const firstChar = text[0].toUpperCase();
   const restString = text.slice(1);
   return `${firstChar}${restString}`;
