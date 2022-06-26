38446890d7e138fbfaa3b75c961ec8de7a42f134
Alexander <the7alexander7@gamil.com>
2022-06-26 20:49:35 +0300

    add asserts

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,10 +1,8 @@
 import capitalize from '../src/capitalize.js';
+import { strict as assert } from 'assert';
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно');
-}
-if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно');
-}
+assert.equal(capitalize('hello'), 'Hello');
+
+assert.equal(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
