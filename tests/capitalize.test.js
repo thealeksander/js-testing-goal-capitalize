822a446b1dd3bbc52da37729fd486cf755d498a6
Alexander <the7alexander7@gamil.com>
2022-06-26 22:00:12 +0300

    upgrate assets

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,8 +1,8 @@
 import capitalize from '../src/capitalize.js';
-import { strict as assert } from 'assert';
+import assert from 'power-assert';
 
-assert.equal(capitalize('hello'), 'Hello');
+assert(capitalize('hello') === 'Hello');
 
-assert.equal(capitalize(''), '');
+assert(capitalize('') === '');
 
 console.log('Все тесты пройдены!');
